de4e12d48cb926f22ae012d961482c64630b1dda
Liam Siegel <liam@siegel.io>
2023-03-10 12:38:09 +0900

    added while loop

--- chain.py
+++ chain.py
@@ -12,6 +12,7 @@ import smtplib
 import imaplib
 from email.message import EmailMessage
 import inspect
+import time
 
 def get_current_date():
     # Get the source code of the function
@@ -19,8 +20,7 @@ def get_current_date():
     d2 = today.strftime("%B %d")
     return d2
 
-
-def get_user_input():
+while True:
     M = imaplib.IMAP4_SSL('imap.gmail.com')
     email_address = "liam@siegel.io"
     EMAIL_Password = os.environ.get('Email_App_Password')
@@ -31,122 +31,138 @@ def get_user_input():
     M.select('inbox')
 
     typ, data = M.search(None, 'SUBJECT "Hey"')
+    if not data[0]:
+        print("no email")
+    else:       
+        def get_user_input():
+            M = imaplib.IMAP4_SSL('imap.gmail.com')
+            email_address = "liam@siegel.io"
+            EMAIL_Password = os.environ.get('Email_App_Password')
+            password = EMAIL_Password
 
-    email_id = data[0]
+            M.login(email_address,password)
 
-    result , email_data = M.fetch(email_id, '(RFC822)')
+            M.select('inbox')
 
-    raw_email = email_data[0][1]
+            typ, data = M.search(None, 'SUBJECT "Hey"')
 
-    raw_email_string = raw_email.decode('utf-8')
+            email_id = data[0]
 
-    email_message = email.message_from_string(raw_email_string)
+            result , email_data = M.fetch(email_id, '(RFC822)')
 
-    for part in email_message.walk():
-        
-        if part.get_content_type() == 'text/plain':
-            body = part.get_payload(decode=True)
-            text_email = body
+            raw_email = email_data[0][1]
 
-    # bytes object
-    b_name = text_email
+            raw_email_string = raw_email.decode('utf-8')
 
-    # convert to string and remove b''
-    name = b_name.decode()
-    name = re.sub("^b'|'$", '', name)
+            email_message = email.message_from_string(raw_email_string)
 
-    print(name)
-    
-    return name
+            for part in email_message.walk():
+                
+                if part.get_content_type() == 'text/plain':
+                    body = part.get_payload(decode=True)
+                    text_email = body
 
+            # bytes object
+            b_name = text_email
 
-current_date = get_current_date()
-temp = f"Today is: {current_date}. Reword this to have in account for the current date: "
+            # convert to string and remove b''
+            name = b_name.decode()
+            name = re.sub("^b'|'$", '', name)
 
-llm = OpenAI(model_name='gpt-3.5-turbo-0301', temperature=0)
-prompt = PromptTemplate(
-    input_variables=["product"],
-    template = f"{temp}" + "{product}?",
-)
-
-from langchain.chains import LLMChain
-chain = LLMChain(llm=llm, prompt=prompt)
-
-# Run the chain only specifying the input variable.
-# print(chain.run("tommorrow at 9pm to 11pm"))
-
-second_prompt = PromptTemplate(
-    input_variables=["company_name"],
-    template="Write this {company_name}, in the form: (Month day 0:00-0:00)",
-)
-chain_two = LLMChain(llm=llm, prompt=second_prompt)
-
-third_prompt = PromptTemplate(
-    input_variables=["company_name"],
-    template="Make sure that: {company_name} only includes (Month day 0:00-0:00)",
-)
-chain_three = LLMChain(llm=llm, prompt=third_prompt)
-
-fourth_prompt = PromptTemplate(
-    input_variables=["company_name"],
-    template="Make sure there are no commas or periods in: {company_name}",
-)
-chain_four = LLMChain(llm=llm, prompt=fourth_prompt)
-
-from langchain.chains import SimpleSequentialChain
-overall_chain = SimpleSequentialChain(chains=[chain, chain_two, chain_three, chain_four], verbose=True)
-
-# Run the chain specifying only the input variable for the first chain.
-user_input = get_user_input()
-catchphrase = overall_chain.run(user_input)
-end = catchphrase
+            print(name)
+            
+            return name
 
 
-import os
-import smtplib
-import imaplib
-import email
-from email.message import EmailMessage
+        current_date = get_current_date()
+        temp = f"Today is: {current_date}. Reword this to have in account for the current date: "
 
-EMAIL_Password = os.environ.get('Email_App_Password')
+        llm = OpenAI(model_name='gpt-3.5-turbo-0301', temperature=0)
+        prompt = PromptTemplate(
+            input_variables=["product"],
+            template = f"{temp}" + "{product}?",
+        )
 
-# Connect to IMAP server and select the inbox
-imap_server = imaplib.IMAP4_SSL('imap.gmail.com')
-imap_server.login('liam@siegel.io', EMAIL_Password)
-imap_server.select('inbox')
+        from langchain.chains import LLMChain
+        chain = LLMChain(llm=llm, prompt=prompt)
 
-# Search for emails with the subject "Hey"
-status, response = imap_server.search(None, 'SUBJECT "Hey"')
+        # Run the chain only specifying the input variable.
+        # print(chain.run("tommorrow at 9pm to 11pm"))
 
-# Get the message IDs of the matching emails
-message_ids = response[0].split()
+        second_prompt = PromptTemplate(
+            input_variables=["company_name"],
+            template="Write this {company_name}, in the form: (Month day 0:00-0:00)",
+        )
+        chain_two = LLMChain(llm=llm, prompt=second_prompt)
 
-if not message_ids:
-    print("No matching emails found.")
-    exit()
+        third_prompt = PromptTemplate(
+            input_variables=["company_name"],
+            template="Make sure that: {company_name} only includes (Month day 0:00-0:00)",
+        )
+        chain_four = LLMChain(llm=llm, prompt=third_prompt)
 
-# Select the last matching email (most recent)
-latest_message_id = message_ids[-1]
-status, response = imap_server.fetch(latest_message_id, "(RFC822)")
+        fourth_prompt = PromptTemplate(
+            input_variables=["company_name"],
+            template="Make sure there are no commas or periods in: {company_name}",
+        )
+        chain_three = LLMChain(llm=llm, prompt=fourth_prompt)
 
-# Parse the email message
-raw_email = response[0][1]
-email_message = email.message_from_bytes(raw_email)
+        from langchain.chains import SimpleSequentialChain
+        overall_chain = SimpleSequentialChain(chains=[chain, chain_two, chain_three, chain_four], verbose=True)
 
-# Create a reply message
-reply_message = EmailMessage()
-reply_message['Subject'] = f"Re: {email_message['Subject']}"
-reply_message['From'] = "Liam@siegel.io"
-reply_message['To'] = email_message['From']
-reply_message['In-Reply-To'] = email_message['Message-ID']
-reply_message['References'] = email_message['Message-ID']
-reply_message.set_content(f'did you mean {end}?')
+        # Run the chain specifying only the input variable for the first chain.
+        user_input = get_user_input()
+        catchphrase = overall_chain.run(user_input)
+        end = catchphrase
 
-# Send the reply message
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login("liam@siegel.io", EMAIL_Password)
-    smtp.send_message(reply_message)
 
-# Close the IMAP connection
-imap_server.close()
-imap_server.logout()
+        import os
+        import smtplib
+        import imaplib
+        import email
+        from email.message import EmailMessage
+
+        EMAIL_Password = os.environ.get('Email_App_Password')
+
+        # Connect to IMAP server and select the inbox
+        imap_server = imaplib.IMAP4_SSL('imap.gmail.com')
+        imap_server.login('liam@siegel.io', EMAIL_Password)
+        imap_server.select('inbox')
+
+        # Search for emails with the subject "Hey"
+        status, response = imap_server.search(None, 'SUBJECT "Hey"')
+
+        # Get the message IDs of the matching emails
+        message_ids = response[0].split()
+
+        if not message_ids:
+            print("No matching emails found.")
+            exit()
+
+        # Select the last matching email (most recent)
+        latest_message_id = message_ids[-1]
+        status, response = imap_server.fetch(latest_message_id, "(RFC822)")
+
+        # Parse the email message
+        raw_email = response[0][1]
+        email_message = email.message_from_bytes(raw_email)
+
+        # Create a reply message
+        reply_message = EmailMessage()
+        reply_message['Subject'] = f"Re: {email_message['Subject']}"
+        reply_message['From'] = "Liam@siegel.io"
+        reply_message['To'] = email_message['From']
+        reply_message['In-Reply-To'] = email_message['Message-ID']
+        reply_message['References'] = email_message['Message-ID']
+        reply_message.set_content(f'did you mean {end}?')
+
+        # Send the reply message
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login("liam@siegel.io", EMAIL_Password)
+            smtp.send_message(reply_message)
+
+        # Close the IMAP connection
+        imap_server.close()
+        imap_server.logout()
+
+    time.sleep(30) 
